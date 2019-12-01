945bad148a73dc27ae2a8bc5f24d1310d68b284d
Anton Biriukov <birtony99@gmail.com>
2019-12-01 00:00:51 -0500

    Added initial settings page

--- screens/main/settings/Screen.js
+++ screens/main/settings/Screen.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import { Row, Grid } from 'react-native-easy-grid';
 
-export default function Settings() {
+export default function Settings({ navigation }) {
   return (
     <SafeAreaView style={styles.safeAreaViewContainer}>
-      <></>
+      <Grid>
+        <Row>
+          <Text h1 style={styles.h1}>
+            Settings
+          </Text>
+        </Row>
+        <Row>
+          <Button title="User Profile" onPress={() => navigation.navigate('UserProfile')} />
+        </Row>
+        <Row>
+          <Button
+            title="Log Out"
+            onPress={() => {
+              // logout the user
+              navigation.navigate('Login');
+            }}
+          />
+        </Row>
+      </Grid>
     </SafeAreaView>
   );
 }
@@ -12,6 +32,10 @@ export default function Settings() {
 const styles = StyleSheet.create({
   safeAreaViewContainer: {
     flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     backgroundColor: '#F28E00',
   },
 });
